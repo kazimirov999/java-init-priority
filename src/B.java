cce3b814dba630fd90bf8653830ae59b451d3fa5
kuchmei <1ki15b.kuchmei@gmail.com>
2019-06-20 11:38:02 +0300

    Add constructor, static and no-static blocks.

---
+++ src/B.java
@@ -0,0 +1,13 @@
+public class B extends A {
+    public B() {
+        System.out.println("Call constructor in class B");
+    }
+    static {
+        System.out.println("Call static block initilization in class B");
+
+    }
+    {
+        System.out.println("Call no-static block initilization in class B");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        C c = new C();
+    }
+}
